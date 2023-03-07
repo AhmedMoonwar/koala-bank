d582748f5d2d607d2772b2a0117d85e313b3b230
AhmedMoonwar <rafidahmed125@gmail.com>
2023-03-07 19:03:24 +0600

    transaction area java added half

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Login Button Event Handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function(){
     const loginArea = document.getElementById("login-area");
@@ -5,3 +6,34 @@ loginBtn.addEventListener("click", function(){
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
 })
+
+// Deposite Button Event Handler
+const depositeBtn = document.getElementById("deposite-btn");
+depositeBtn.addEventListener("click", function(){
+    const depositeAmountNum = getInput("deposite-amount");
+
+    balanceUpdate("deposite-Amount", depositeAmountNum);
+    balanceUpdate("balance-amount", depositeAmountNum);
+
+    document.getElementById("deposite-amount").value = "";
+})
+
+// Withdraw Button Event Handler
+const withdrawBtn = document.getElementById("withdraw-btn");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawAmountNum = getInput("withdraw-amount");
+    console.log(withdrawAmountNum);
+})
+
+function getInput(id){
+    const Amount = document.getElementById(id).value;
+    const AmountNum = parseFloat(Amount);
+    return AmountNum;
+}
+
+function balanceUpdate(id, depositeAmountNum){
+    const currentAmount = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(currentAmount);
+    const totalAmount = depositeAmountNum + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
